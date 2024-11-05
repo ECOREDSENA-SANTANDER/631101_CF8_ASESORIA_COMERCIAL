7248477de460594e9e69c3734151305ecbdb5d36
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-11-05 15:46:14 -0500

    Texto alternativo sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.png", alt="falta en el DI Y XD")
+          img(src="@/assets/curso/temas/sintesis.png", alt="El servicio postventa es clave para atraer y fidelizar clientes, asegurando su satisfacción y lealtad. Este análisis aborda las etapas esenciales del servicio, su estructuración en la empresa, las herramientas para implementarlo y los distintos tipos de servicios disponibles, con el objetivo de ofrecer una experiencia de calidad y duradera.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
